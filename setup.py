08d0534b62cb3bdffd3e9cd5a8e345435930b6a2
Enric Lluelles <enric@lluell.es>
2016-11-08 23:15:30 +0100

    Fix wrong readme ref, bump version, license

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='troposphere_sugar',
-    version='0.0.2',
+    name='troposphere-sugar',
+    version='0.0.3',
     description='Common utilities on top of troposphere and boto for ease of clouformation template creation',
     author='Enric Lluelles',
     author_email='enric@lluel.es',
     url="https://github.com/enriclluelles/troposphere_sugar",
     packages=['troposphere_sugar', 'troposphere_sugar.decorators'],
+    license='MIT',
     install_requires=['troposphere>=1.2.0']
 )
