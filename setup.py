1f9c998815ddcb1ededb940ebc86ac816d2cf6aa
Enric Lluelles <enric@lluell.es>
2016-11-10 23:26:30 +0100

    Add boto3 as dependency, bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='troposphere-sugar',
-    version='0.0.4',
+    version='0.0.5',
     description='Common utilities on top of troposphere and boto for ease of clouformation template creation',
     author='Enric Lluelles',
     author_email='enric@lluel.es',
     url="https://github.com/enriclluelles/troposphere_sugar",
     packages=['troposphere_sugar', 'troposphere_sugar.decorators'],
     license='MIT',
-    install_requires=['troposphere>=1.2.0']
+    install_requires=['troposphere>=1.2.0', 'boto3>=1.4.0']
 )
